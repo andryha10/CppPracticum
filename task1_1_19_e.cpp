8ceabc797d5a347816bab8d5e6ed7813f4ed43b4
andryha10 <53227681+andryha10@users.noreply.github.com>
2020-10-18 15:38:44 +0200

    Dz 3
    Kodovbetskyi Dz 3

---
+++ task1_1_19_e.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+double isRelu(int a, int x) {
+	if (x < 0) { return x / (sqrt(1 + a * x * x)); }
+	else { return x; }
+}
+double deriv(int a, int x) {
+	if (isRelu(a, x) == x) { return 1; }
+	else { return (sqrt(1 + a * x * x) - ((2 * a * x * x) / sqrt(1 + a * x * x))) / (1 + a * x * x); }
+}
+int main() {
+	int a, x;
+	printf("Enter a,x:\n");
+	cin >> a >> x;
+	cout << "\nDerivative = " << deriv(a, x);
+
+}
---
+++ task1_1_7_b.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main1() {
+	int a, b, c;
+	double x1, x2, D;
+	printf("Enter a, b, c:\n");
+	cin >> a >> b >> c;
+
+	D = b * b - (4 * a * c);
+	cout <<"D: "<< D;
+	if (D < 0) { cout << "\nNo solution"; }
+	if (D == 0) {
+		double x1 = -b/ (2 * a);
+		if (x1 > 0) { cout << "\n2 solutiuns :" << sqrt(x1) << ", " << -sqrt(x1); }
+		else { cout << "\nNo solutions"; }
+	}
+
+	if (D > 0) {
+		x1 = (-b + sqrt(D)) / (2 * a);
+		x2 = (-b - sqrt(D)) / (2 * a);
+		if ((x1 > 0) && (x2 > 0))  {cout << "\n4 solutions: " << x1 <<", "<< -x1 <<", "<< x2<<", " << -x2;}
+		if ((x1 > 0) && (x2 <= 0)) {cout << "\n2 solutions: " << x1 <<", " << -x1;}
+		if ((x1 <= 0) && (x2 > 0)) {cout << "\n2 solutions: " << x2 <<", "<< -x2;}
+		if ((x1 < 0) && (x2 < 0)) { cout << "\nNo solutions: "; }
+	}
+	return 0;
+}
